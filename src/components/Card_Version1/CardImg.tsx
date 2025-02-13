530cb0caf2053eec3181db90d344484d7c3f8bb1
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-02-13 16:59:33 +0100

    Cours...

--- src/components/Card_Version1/CardImg.tsx
+++
@@ -1,9 +0,0 @@
-export function CardImg() {
-    return (
-        <figure>
-            <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes" />
-        </figure>
-    )
-}
